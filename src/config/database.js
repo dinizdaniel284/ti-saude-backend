819e9602de6e7905fa0c0ca7e985d8e7f04d0411
dinizdaniel284 <dinizdaniel284@gmail.com>
2025-12-24 12:25:47 -0300

    fix: change MONGO_URI to MONGODB_URI

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const conectarMongo = async () => {
-  if (!process.env.MONGO_URI) {
-    console.error("❌ Erro: MONGO_URI não está definido.");
+  if (!process.env.MONGODB_URI) {
+    console.error("❌ Erro: MONGODB_URI não está definido.");
     process.exit(1);
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("✅ Conectado ao MongoDB");
   } catch (err) {
     console.error("❌ Erro ao conectar ao MongoDB:", err);
